f6ae889d4619ef7875dbfc7d1822740b857d6879
Stefano <stefano.pola.fargas@gmail.com>
2021-01-06 12:17:51 +0100

    Actualitzat el metode hello

--- src/main/java/com/example/demo/ControllerHello.java
+++ src/main/java/com/example/demo/ControllerHello.java
@@ -5,6 +5,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
@@ -18,18 +19,21 @@ import org.springframework.ui.Model;
 public class ControllerHello {
 	
 	 /**
-	  * Metode que mapeja l'endpoint /hello des d' index.html (carpeta static) i retorna al navegador l'arxiu hello.html (carpeta templates)
+	  * Metode que mapeja l'endpoint /hello/{name} des de l' index.html (carpeta static) i retorna al navegador l'arxiu hello.html amb el nom (carpeta templates)
 	  * 
 	 * @param model
 	 * @return  hello.html
 	 */
 	
-	@GetMapping("/hello")
-	 //@ResponseBody
-	    String hello(Model model) {
-	        return "hello";
-	 }
+		@GetMapping("/hello/{name}")
+		// @ResponseBody
+		String hello(@PathVariable("name") String name, Model model) {
+
+			model.addAttribute("name", name);
+			return "hello";
+		}
 }
+
 
 
 
